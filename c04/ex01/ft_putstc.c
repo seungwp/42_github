06dbb97366ffa000d8e68c82a47124c16c786b2d
seukim <kimseungwp@naver.com>
2025-11-05 21:19:00 +0900

    m

--- c04/ex01/ft_putstc.c
+++ c04/ex01/ft_putstc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 22:30:20 by seukim            #+#    #+#             */
-/*   Updated: 2025/11/05 11:34:11 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/05 21:18:30 by seukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,9 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
 }
-
-// int	main(void)
-// {
-// 	char string[] = "adfasdf";
-// 	ft_putstr(string);
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
