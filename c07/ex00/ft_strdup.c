fbb5afb1220eb2c589003a866734240607f07e9e
seungwp <kimseungwp@naver.com>
2025-11-08 16:47:44 +0900

    c06 c07 ex01까지

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -3,10 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:05:33 by seukim            #+#    #+#             */
-/*   Updated: 2025/11/06 21:05:34 by seukim           ###   ########.fr       */
+/*   Updated: 2025/11/08 16:45:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*dup;
+	int		i;
+
+	len = ft_strlen(src);
+	dup = (char *)malloc(len + 1);
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+// #include <stdio.h>
+// int main(void) 
+// {
+//     char *src = "hello1234";
+//     char *dup = ft_strdup(src);
+//     printf("%s\n", dup);
+//     free(dup);
+//     return 0;
+// }
--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -3,10 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:05:35 by seukim            #+#    #+#             */
-/*   Updated: 2025/11/06 21:05:36 by seukim           ###   ########.fr       */
+/*   Updated: 2025/11/08 16:47:08 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int		*result;
+	int		range;
+	int		i;
+
+	i = 0;
+	range = max - min;
+	if (range <= 0)
+		return (NULL);
+	result = (int *)malloc(sizeof(int) * range);
+	if (result == NULL)
+		return (NULL);
+	while (min < max)
+	{
+		result[i] = min;
+		i++;
+		min++;
+	}
+	return (result);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	int min = 1;
+// 	int max = 10;
+// 	int *arr = ft_range(min, max);
+
+//     if (arr == NULL)
+//     {
+//         printf("error!\n");
+//         return 1;
+//     }
+
+//     for (int i = 0; i < max - min; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+
+//     free(arr);
+//     return 0;
+// }
