b30daca29d624c0fcc7422c63131d7fd4e4c5433
seukim <kimseungwp@naver.com>
2026-01-26 21:37:08 +0900

    adasf

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,8 +6,42 @@
 /*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 16:00:39 by seukim            #+#    #+#             */
-/*   Updated: 2025/12/22 21:28:35 by seukim           ###   ########.fr       */
+/*   Updated: 2026/01/26 21:31:25 by seukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	is_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (ft_strdup(""));
+	if (!set)
+		return (ft_strdup(""));
+	start = 0;
+	end = ft_strlen(s1);
+	while (is_set(s1[start], set))
+		start++;
+	if (start == end)
+		return (ft_strdup(""));
+	while (is_set(s1[end - 1], set))
+		end--;
+	return (ft_substr(s1, start, end - start));
+}
