9bd513fa1174463a7e762298037df749141dcd22
seukim <kimseungwp@naver.com>
2026-01-27 17:07:14 +0900

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,8 +6,78 @@
 /*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 16:00:23 by seukim            #+#    #+#             */
-/*   Updated: 2025/12/22 21:28:35 by seukim           ###   ########.fr       */
+/*   Updated: 2026/01/27 17:06:47 by seukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	count_words(const char *s, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	**free_all(char **result, int i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(result[i]);
+	}
+	free(result);
+	return (NULL);
+}
+
+static char	**fill_res(char const *s, char c, char **res)
+{
+	int	i;
+	int	j;
+	int	start;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			res[j] = ft_substr(s, start, i - start);
+			if (!res[j++])
+				return (free_all(res, j - 1));
+		}
+	}
+	res[j] = NULL;
+	return (res);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+
+	if (!s)
+		return (NULL);
+	res = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!res)
+		return (NULL);
+	return (fill_res(s, c, res));
+}
--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,8 +6,28 @@
 /*   By: seukim <seukim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 16:00:34 by seukim            #+#    #+#             */
-/*   Updated: 2025/12/22 21:28:35 by seukim           ###   ########.fr       */
+/*   Updated: 2026/01/27 17:05:31 by seukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*new_mem;
+	unsigned int	i;
+
+	i = 0;
+	if (s == 0 || f == 0)
+		return (NULL);
+	new_mem = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!new_mem)
+		return (NULL);
+	while (s[i])
+	{
+		new_mem[i] = f(i, s[i]);
+		i++;
+	}
+	new_mem[i] = '\0';
+	return (new_mem);
+}
